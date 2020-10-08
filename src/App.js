65c5cca6224548c89c4205d00fb96973843777c8
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2020-10-08 18:22:16 +0500

    Layout and Sidebar Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Whatsapp</h1>
+    <div className="app">
+      <div className="app__body">
+        <Sidebar />
+      </div>
     </div>
   );
 }
---
+++ src/components/SidebarChat/SidebarChat.js
@@ -0,0 +1,14 @@
+import { Avatar } from '@material-ui/core';
+import React from 'react';
+import './SidebarChat.css';
+
+function SidebarChat() {
+    return (
+        <div className="sidebarChat">
+            <Avatar />
+            <h3>Malik Waqar</h3>
+        </div>
+    )
+}
+
+export default SidebarChat;
